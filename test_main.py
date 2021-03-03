c4e70ffa95bedf74c0080ea57b7cad88bb07d4b7
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 20:49:01 +0000

    リファクタリング、準備について関数の抽出

--- test_main.py
+++ test_main.py
@@ -2,20 +2,18 @@ from unittest import TestCase
 from main import FizzBuzz
 
 class TestFizzBuzz(TestCase):
-    def test_1を渡すと文字列1を返す(self):
+    def setUp(self):
         # 準備
-        fizzbuzz = FizzBuzz()
+        self.fizzbuzz = FizzBuzz()
+
+    def test_1を渡すと文字列1を返す(self):
         # 実行＆検証
-        self.assertEqual(fizzbuzz.convert(1), "1")
+        self.assertEqual(self.fizzbuzz.convert(1), "1")
 
     def test_2を渡すと文字列2を返す(self):
-        # 準備
-        fizzbuzz = FizzBuzz()
         # 実行＆検証
-        self.assertEqual(fizzbuzz.convert(2), "2")
+        self.assertEqual(self.fizzbuzz.convert(2), "2")
 
     def test_3を渡すと文字列Fizzを返す(self):
-        # 準備
-        fizzbuzz = FizzBuzz()
         # 実行＆検証
-        self.assertEqual(fizzbuzz.convert(3), "Fizz")
+        self.assertEqual(self.fizzbuzz.convert(3), "Fizz")
