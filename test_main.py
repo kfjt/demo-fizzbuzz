5462d20ea2b242356beb90394b836396f47b3fc7
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 20:48:49 +0000

    Fizzについてテスト追加

--- test_main.py
+++ test_main.py
@@ -13,3 +13,9 @@ class TestFizzBuzz(TestCase):
         fizzbuzz = FizzBuzz()
         # 実行＆検証
         self.assertEqual(fizzbuzz.convert(2), "2")
+
+    def test_3を渡すと文字列Fizzを返す(self):
+        # 準備
+        fizzbuzz = FizzBuzz()
+        # 実行＆検証
+        self.assertEqual(fizzbuzz.convert(3), "Fizz")
