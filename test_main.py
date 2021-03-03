d72416c02dd5200c35907c9fb84074fc911b79a0
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 21:37:22 +0000

    コメント除去

--- test_main.py
+++ test_main.py
@@ -5,20 +5,14 @@ class TestFizzBuzz:
     class Test_convertメソッドは数を文字列に変換する:
         class _3の倍数のときは数の代わりにFizzに変換する(TestCase):
             def test_3を渡すと文字列Fizzを返す(self):
-                # 準備
-                # 実行＆検証
                 self.assertEqual(FizzBuzz().convert(3), "Fizz")
 
         class _5の倍数のときは数の代わりにBuzzに変換する(TestCase):
             def test_5を渡すと文字列Buzzを返す(self):
-                # 準備
-                # 実行＆検証
                 self.assertEqual(FizzBuzz().convert(5), "Buzz")
 
         class その他の数のときはそのまま文字列に変換する(TestCase):
             def test_1を渡すと文字列1を返す(self):
-                # 準備
-                # 実行＆検証
                 self.assertEqual(FizzBuzz().convert(1), "1")
 
 if __name__ == "__main__":
