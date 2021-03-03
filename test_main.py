6b63b85b869c1d2cfb68bf6ad819cb5ab2601fcf
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 21:17:50 +0000

    階層化、例 subTest だと件数が少なく見える

--- test_main.py
+++ test_main.py
@@ -6,18 +6,21 @@ class TestFizzBuzz(TestCase):
         # 準備
         self.fizzbuzz = FizzBuzz()
 
-    def test_1を渡すと文字列1を返す(self):
-        # 実行＆検証
-        self.assertEqual(self.fizzbuzz.convert(1), "1")
+    def test_数を文字列に変換する(self):
+        with self.subTest("1を渡すと文字列1を返す"):
+            # 実行＆検証
+            self.assertEqual(self.fizzbuzz.convert(1), "1")
 
-    def test_2を渡すと文字列2を返す(self):
-        # 実行＆検証
-        self.assertEqual(self.fizzbuzz.convert(2), "2")
+        with self.subTest("2を渡すと文字列2を返す"):
+            # 実行＆検証
+            self.assertEqual(self.fizzbuzz.convert(2), "2")
 
-    def test_3を渡すと文字列Fizzを返す(self):
-        # 実行＆検証
-        self.assertEqual(self.fizzbuzz.convert(3), "Fizz")
+    def test_3の倍数のときは数の代わりにFizzに変換する(self):
+        with self.subTest("3を渡すと文字列Fizzを返す"):
+            # 実行＆検証
+            self.assertEqual(self.fizzbuzz.convert(3), "Fizz")
 
-    def test_5を渡すと文字列Buzzを返す(self):
-        # 実行＆検証
-        self.assertEqual(self.fizzbuzz.convert(5), "Buzz")
+    def test_5の倍数のときは数の代わりにBuzzに変換する(self):
+        with self.subTest("5を渡すと文字列Buzzを返す"):
+            # 実行＆検証
+            self.assertEqual(self.fizzbuzz.convert(5), "Buzz")
