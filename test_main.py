ba4f8734ae732f9ba452b3a88e0ae87ef2cb7148
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 20:48:06 +0000

    リファクタリング、変数のインライン化

--- test_main.py
+++ test_main.py
@@ -5,7 +5,5 @@ class TestFizzBuzz(TestCase):
     def test_1を渡すと文字列1を返す(self):
         # 準備
         fizzbuzz = FizzBuzz()
-        # 実行
-        actual = fizzbuzz.convert(1)
-        # 検証
-        self.assertEqual(actual, "1")
+        # 実行＆検証
+        self.assertEqual(fizzbuzz.convert(1), "1")
