03f42608c2e2f8c9630c56d4b1670f8bcaec79ea
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 20:47:54 +0000

    実行したいクラス、関数を想定する

--- test_main.py
+++ test_main.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
-
+from main import FizzBuzz
 
 class TestFizzBuzz(TestCase):
     def test_1を渡すと文字列1を返す(self):
         # 準備
+        fizzbuzz = FizzBuzz()
         # 実行
-        actual = None
+        actual = fizzbuzz.convert(1)
         # 検証
         self.assertEqual(actual, "1")
