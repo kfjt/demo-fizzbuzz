f8e2b7057a9387096745294bb946ac7cfd375158
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 20:47:50 +0000

    期待値が明らか

--- test_main.py
+++ test_main.py
@@ -2,9 +2,9 @@ from unittest import TestCase
 
 
 class TestFizzBuzz(TestCase):
-    def test_数を文字列に変換する(self):
+    def test_1を渡すと文字列1を返す(self):
         # 準備
         # 実行
+        actual = None
         # 検証
-        # 後片付け
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "1")
