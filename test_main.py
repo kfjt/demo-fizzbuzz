62f151b0835a10b0d063d8b0704492e3210f2913
kfjt <kfjt@users.noreply.github.com>
2021-03-03 20:47:38 +0000

    ４フェーズテスト

--- test_main.py
+++ test_main.py
@@ -2,5 +2,9 @@ from unittest import TestCase
 
 
 class TestFizzBuzz(TestCase):
-    def test(self):
-        self.assertEqual(True, True)
+    def test_数を文字列に変換する(self):
+        # 準備
+        # 実行
+        # 検証
+        # 後片付け
+        self.assertEqual(actual, expected)
