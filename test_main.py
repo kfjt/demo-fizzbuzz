67c8c08b77c026e054062905a830bd86c240e352
kfjt <k.fujita@jcom.home.ne.jp>
2021-03-03 21:36:59 +0000

    クラス変数の必要が無い、変数のインライン化

--- test_main.py
+++ test_main.py
@@ -6,23 +6,20 @@ class TestFizzBuzz:
         class _3の倍数のときは数の代わりにFizzに変換する(TestCase):
             def test_3を渡すと文字列Fizzを返す(self):
                 # 準備
-                self.fizzbuzz = FizzBuzz()
                 # 実行＆検証
-                self.assertEqual(self.fizzbuzz.convert(3), "Fizz")
+                self.assertEqual(FizzBuzz().convert(3), "Fizz")
 
         class _5の倍数のときは数の代わりにBuzzに変換する(TestCase):
             def test_5を渡すと文字列Buzzを返す(self):
                 # 準備
-                self.fizzbuzz = FizzBuzz()
                 # 実行＆検証
-                self.assertEqual(self.fizzbuzz.convert(5), "Buzz")
+                self.assertEqual(FizzBuzz().convert(5), "Buzz")
 
         class その他の数のときはそのまま文字列に変換する(TestCase):
             def test_1を渡すと文字列1を返す(self):
                 # 準備
-                self.fizzbuzz = FizzBuzz()
                 # 実行＆検証
-                self.assertEqual(self.fizzbuzz.convert(1), "1")
+                self.assertEqual(FizzBuzz().convert(1), "1")
 
 if __name__ == "__main__":
     from unittest import main
